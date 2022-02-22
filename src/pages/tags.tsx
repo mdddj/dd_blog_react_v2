c1502fe9eb2bbf244da496322487b0438b47f818
mdddj <413153189@qq.com>
2022-02-22 15:06:52 +0800

    app: add components

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {useRecoilValue} from "recoil";
 import {archivesDataState} from "../providers/archives";
 import {Box, Heading, HStack, Tag} from "@chakra-ui/react";
+import {Link} from "react-router-dom";
 
 //标签列表页面
 const TagsPage:React.FC = () => {
@@ -10,12 +11,12 @@ const TagsPage:React.FC = () => {
   const tags = useRecoilValue(archivesDataState)?.tags??[]
   console.log(tags)
   return <>
-    <Box p={12} bg={'black'} width={'100%'}>
+    <Box p={12} border={1} width={'100%'}>
       <Heading>标签</Heading>
-      <HStack spacing={4}>
+      <HStack spacing={4} mt={4}>
         {
           tags.map(value => {
-            return <Tag key={value.id}>{value.name}</Tag>
+            return <Tag key={value.id}><Link to={'/tag/'+value.id}>{value.name}</Link></Tag>
           })
         }
       </HStack>
